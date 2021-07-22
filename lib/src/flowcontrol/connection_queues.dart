797cb3f49f08f520dfdcfa0e5816952c3978fc6b
David B. Robins <david.robins@jci.com>
2021-07-22 09:43:53 -0400

    Count ignored DATA frame as processed for window update.

--- lib/src/flowcontrol/connection_queues.dart
+++ lib/src/flowcontrol/connection_queues.dart
@@ -260,7 +260,9 @@ class ConnectionMessageQueueIn extends Object
   /// If a [DataFrame] will be ignored, this method will take the minimal
   /// action necessary.
   void processIgnoredDataFrame(DataFrame frame) {
+    // decrement/restore window - need to generate window update
     _windowUpdateHandler.gotData(frame.bytes.length);
+    _windowUpdateHandler.dataProcessed(frame.bytes.length);
   }
 
   /// Processes an incoming [HeadersFrame] which is addressed to a specific
